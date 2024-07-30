c7aedbbf3df2d57052941c894932ff6ac54830bc
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 22:33:04 +0530

    Add files via upload

---
+++ Week_1_DPP/9.Command Pattern/RemoteControl.java
@@ -0,0 +1,16 @@
+package java_fse_CTS;
+
+//RemoteControl.java
+public class RemoteControl {
+ private Command command;
+
+ public void setCommand(Command command) {
+     this.command = command;
+ }
+
+ public void pressButton() {
+     command.execute();
+ }
+}
+
+
