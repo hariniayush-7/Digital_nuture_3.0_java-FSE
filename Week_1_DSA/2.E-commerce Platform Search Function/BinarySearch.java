83cc4b0aa97f7600d47e833508bd9de3db8590b8
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 20:25:10 +0530

    Add files via upload

---
+++ Week_1_DSA/2.E-commerce Platform Search Function/BinarySearch.java
@@ -0,0 +1,30 @@
+package java_fse_CTS;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BinarySearch {
+    public static Product binarySearch(Product[] products, String targetProductId) {
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = products[mid].getProductId().compareTo(targetProductId);
+
+            if (comparison == 0) {
+                return products[mid];
+            } else if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    public static void sortProductsById(Product[] products) {
+        Arrays.sort(products, Comparator.comparing(Product::getProductId));
+    }
+}
+
