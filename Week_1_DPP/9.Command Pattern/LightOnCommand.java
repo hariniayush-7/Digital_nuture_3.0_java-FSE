8be267a5625da8f71f90cc41513f157dedb2f559
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 22:32:21 +0530

    Add files via upload

---
+++ Week_1_DPP/9.Command Pattern/LightOnCommand.java
@@ -0,0 +1,30 @@
+package java_fse_CTS;
+//LightOnCommand.java
+public class LightOnCommand implements Command {
+ private Light light;
+
+ public LightOnCommand(Light light) {
+     this.light = light;
+ }
+
+ @Override
+ public void execute() {
+     light.turnOn();
+ }
+}
+
+//LightOffCommand.java
+ class LightOffCommand implements Command {
+ private Light light;
+
+ public LightOffCommand(Light light) {
+     this.light = light;
+ }
+
+ @Override
+ public void execute() {
+     light.turnOff();
+ }
+}
+
+
