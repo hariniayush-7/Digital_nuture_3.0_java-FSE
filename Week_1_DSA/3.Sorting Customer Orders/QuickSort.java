2aa55edd3532d4b24b013a6c5d882d8edcc611b1
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 20:29:54 +0530

    Add files via upload

---
+++ Week_1_DSA/3.Sorting Customer Orders/QuickSort.java
@@ -0,0 +1,37 @@
+package java_fse_CTS;
+
+public class QuickSort {
+	
+	    public static void quickSort(Order[] orders, int low, int high) {
+	        if (low < high) {
+	            int pi = partition(orders, low, high);
+
+	            quickSort(orders, low, pi - 1);
+	            quickSort(orders, pi + 1, high);
+	        }
+	    }
+
+	    private static int partition(Order[] orders, int low, int high) {
+	        double pivot = orders[high].getTotalPrice();
+	        int i = (low - 1); // index of smaller element
+	        for (int j = low; j < high; j++) {
+	            if (orders[j].getTotalPrice() <= pivot) {
+	                i++;
+
+	                // swap orders[i] and orders[j]
+	                Order temp = orders[i];
+	                orders[i] = orders[j];
+	                orders[j] = temp;
+	            }
+	        }
+
+	        // swap orders[i+1] and orders[high] (or pivot)
+	        Order temp = orders[i + 1];
+	        orders[i + 1] = orders[high];
+	        orders[high] = temp;
+
+	        return i + 1;
+	    }
+	}
+
+
