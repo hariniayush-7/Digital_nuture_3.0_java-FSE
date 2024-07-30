90acfcc732895d28474507096804093625c6407a
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 22:04:00 +0530

    Add files via upload

---
+++ Week_1_DPP/5.Decorator Pattern/EmailNotifier.java
@@ -0,0 +1,11 @@
+package java_fse_CTS;
+
+//EmailNotifier.java
+public class EmailNotifier implements Notifier {
+ @Override
+ public void send(String message) {
+     System.out.println("Sending Email with message: " + message);
+ }
+}
+
+
