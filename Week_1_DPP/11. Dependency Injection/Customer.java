cbcb837ce79cd4342bdb35354fba85f901b638db
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 22:38:58 +0530

    Add files via upload

---
+++ Week_1_DPP/11. Dependency Injection/Customer.java
@@ -0,0 +1,22 @@
+package java_fse_CTS;
+public class Customer {
+    private String id;
+    private String name;
+
+    // Constructor
+    public Customer(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
