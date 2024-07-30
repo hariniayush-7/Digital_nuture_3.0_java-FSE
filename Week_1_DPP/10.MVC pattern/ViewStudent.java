751efaacb898f38175528fb105606571b01afda7
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 21:05:31 +0530

    Add files via upload

---
+++ Week_1_DPP/10.MVC pattern/ViewStudent.java
@@ -0,0 +1,14 @@
+package java_fse_CTS;
+
+public class ViewStudent {
+	
+	    public void displayStudentDetails(String studentName, String studentId, String studentGrade) 
+	    {
+	        System.out.println("Student: ");
+	        System.out.println("Name: " + studentName);
+	        System.out.println("ID: " + studentId);
+	        System.out.println("Grade: " + studentGrade);
+	    }
+	}
+
+
