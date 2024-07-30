ab5646f63fc5f8b1fdb05482544ebf4d9f5db2e1
hariniayush-7 <140332509+hariniayush-7@users.noreply.github.com>
2024-07-30 22:31:56 +0530

    Add files via upload

---
+++ Week_1_DPP/9.Command Pattern/Light.java
@@ -0,0 +1,19 @@
+package java_fse_CTS;
+//Light.java
+public class Light {
+ private String location;
+
+ public Light(String location) {
+     this.location = location;
+ }
+
+ public void turnOn() {
+     System.out.println(location + " light is ON");
+ }
+
+ public void turnOff() {
+     System.out.println(location + " light is OFF");
+ }
+}
+
+
